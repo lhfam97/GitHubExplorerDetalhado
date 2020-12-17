bb5e7529e6ce97307963767fac659f4fb44c7276
Luique <lhfam97@gmail.com>
2020-12-17 15:14:33 -0300

    Ajustes Padroes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 
-
 function App() {
-  return (
-   <h1>Hello World</h1>
-  );
+    return <h1>Hello World</h1>;
 }
 
 export default App;
