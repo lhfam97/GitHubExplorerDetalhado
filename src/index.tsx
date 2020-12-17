84fef801a4a027a64fe6cdfb5807a2ea472e68f8
Luique <lhfam97@gmail.com>
2020-12-17 15:21:05 -0300

    Ajustes desabilitando regra

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -9,6 +10,3 @@ ReactDOM.render(
     document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
